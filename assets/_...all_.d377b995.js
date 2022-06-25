df095e9475c1529ceb71bb49224557a593e8592c
artiely <1119696785@qq.com>
2022-06-25 17:02:03 +0800

    deploy

---
+++ assets/_...all_.d377b995.js
@@ -0,0 +1,1 @@
+import{d as e,a as o,o as a,c as s,t as c,f as r,j as n}from"./app.1cc11fc1.js";const f=e({__name:"[...all]",setup(l){const{t}=o();return(p,u)=>(a(),s("div",null,c(r(t)("not-found")),1))}});typeof n=="function"&&n(f);export{f as default};
